9991ca7e7a2f15fe5a3af0a4554704bf16021418
Mike <miguetrulo07@gmail.com>
2022-06-09 22:32:26 -0500

    Assert part2

---
+++ addition/sum.py
@@ -0,0 +1,16 @@
+def sum_positive_numbers (n1: int, n2: int) -> int:
+    """It allows to sum tow integers numbers
+
+    arg (int):
+        n1 (int)
+        n2 (int)
+
+    returns:
+        int
+
+    """
+
+    return n1 + n2
+if __name__ == '__main__':
+    result = sum_positive_numbers(10 , 20)
+    print(result)
--- unittest/main.py
+++ unittest/main.py
@@ -2,10 +2,12 @@
 
 if __name__ == '__main__':
     try:
-        #assert 5 == 10, 'Sorry but not sorry 5 is not equal to 10.'
+        assert 5 == 10, 'Sorry but not sorry 5 is not equal to 10.'
+        """
         if not 5 == 10:
-            raise AssertionError("Sorry but not sorry gg five isn't equal to ten")
 
+            raise AssertionError("Sorry but not sorry gg five isn't equal to ten")
+        """
         print('>>> The program continues with his excution.')
 
     except AssertionError as error:
