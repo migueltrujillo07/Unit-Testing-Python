af6060ae66c24132008cae25af187706463dde1e
Mike <miguetrulo07@gmail.com>
2022-06-09 20:14:47 -0500

    Adding user scrip

---
+++ docstring/user.py
@@ -0,0 +1,15 @@
+"""Este es el docstring del modulo main """
+
+# Docstring
+# __Doc__
+
+class User:
+    """ It allow to create a user. """
+    def __init__(self, username: str, password: str)-> None:
+        """ It allows to instance an user object
+        Args: 
+        username (str): User's username
+        password (str): user's password
+        """
+
+
