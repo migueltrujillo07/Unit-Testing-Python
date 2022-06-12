b69556fc97b37fcd8a44e035d2f32ac03b1e1fdf
Mike <miguetrulo07@gmail.com>
2022-06-11 22:12:01 -0500

    setup and teardown

--- pytest/env/test_main.py
+++ pytest/env/test_main.py
@@ -2,9 +2,22 @@ import pytest
 
 class TestExample():
 
+    def setup_method(self):
+        pass
+
+    def teardown_method(self):
+        pass
+
+
     def test_sum_two_numbers(self):
         assert 10 + 10 == 20, 'Sorry the sum is incorrect'
 
 
     def test_subs_two_numbers(self):
         assert 10 - 10 == 0, 'Sorry, the substraccion is incorrect'
+
+class TestExample2():
+
+    def test_mul_two_numberss(self):
+        assert 2 * 10 == 20, 'Sorry but the multiplication is incorrect'
+
