3c79d9420be3a67916a9e92eff34eae41c522128
Mike <miguetrulo07@gmail.com>
2022-06-11 19:25:36 -0500

    Adding related operators

---
+++ relatedOperators/product.py
@@ -0,0 +1,16 @@
+class ProductDiscountError(Exception):
+    pass
+
+
+class Product:
+
+    def __init__(self, name: str, price: float, discount: float = 0.0) -> None:
+        self.name = name
+        self.price = price
+        self.discount = discount
+        
+        if discount > price:
+            raise ProductDiscountError('Sorry but the discount can not be more than the price')
+        
+        
+
